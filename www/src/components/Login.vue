fab2e64c1b7f5b9535e969a5aecde0fb2be7f640
jaime ambriz <jaime.ambriz@yahoo.com>
2017-11-29 13:50:26 -0700

    Login page made

---
+++ www/src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class='login'>
+        <h1>test</h1>
+    </div>
+
+
+</template>
+
+
+<script>
+    export default {
+        name:'login',
+        data() {
+            return {
+
+            }
+        }
+
+    }
+</script>
